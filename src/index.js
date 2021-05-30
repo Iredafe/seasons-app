cec86bc9b11dc68ab94060d292cb55e2a91af43c
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-05-29 23:00:24 -0600

    refactor to class component closes #4

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App=()=>{
-    window.navigator.geolocation.getCurrentPosition(position=>
-        console.log(position),
-        err=>
-            console.log(err)
-    )
-  
-    return <div>Hello there</div>
-}
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={lat:null}
+    }
+    render(){
+        window.navigator.geolocation.getCurrentPosition(position=>{
+            console.log(position);
+            this.setState({lat:position.coords.latitude})},
+            err=>
+                console.log(err)
+        )
+        return <div> Latitude: {this.state.lat} </div>
+
+    };
+    }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
