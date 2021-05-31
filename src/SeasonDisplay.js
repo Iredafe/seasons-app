6546b9251623728e2614dbd8e34efefd4e4e7398
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-05-31 13:56:54 -0600

    determine season closes #10

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+const getSeason=(lat, month)=>{
+if(lat>2 && lat<9){
+    return lat > 0 ? 'summer' : 'winter';
+}else{
+    return lat < 0 ? 'winter' : 'summer';
+}
+}
+
 const SeasonDisplay=(props)=>{
-    console.log(props.lat);
+
+    const season = getSeason(props.lat, new Date().month)
+    console.log(season);
     return <div>Hello Season</div>
 }
 
