264328590eaecd308409f1e1ae7d1d89f9a81203
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-05-31 11:39:41 -0600

    conditionally render content closes #6

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ class App extends React.Component{
     constructor(props){
         super(props);
         this.state={lat:null, errorMessage: ''}
-    }
-    render(){
         window.navigator.geolocation.getCurrentPosition(position=>{
             console.log(position);
             this.setState({lat:position.coords.latitude})},
@@ -14,11 +12,18 @@ class App extends React.Component{
             this.setState({errorMessage: err.message})
 
             )
-        return (
-        <div> Latitude: {this.state.lat} 
-        <br></br>
-        Error: {this.state.errorMessage}</div>
-        )
+       
+    }
+    render(){
+        if(this.state.errorMessage && !this.state.lat){
+            return (<div>Error: {this.state.errorMessage}</div>)
+        }else if(!this.state.errorMessage && this.state.lat){
+            return (
+                <div> Latitude: {this.state.lat}</div>
+                );
+        } else{
+            return <div>Loading!</div>
+        }
     };
     }
 
